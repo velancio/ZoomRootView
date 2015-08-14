09156c6c6a01990236705ff4214343eea8702510
Velancio Fernandes <velancio@gmail.com>
2015-08-14 15:27:03 -0700

    Modifications to original code base

--- app/src/main/java/com/velanciofernandes/demozoomapp/LoginActivity.java
+++ app/src/main/java/com/velanciofernandes/demozoomapp/LoginActivity.java
@@ -1,62 +1,52 @@
 package com.velanciofernandes.demozoomapp;
 
-
 import android.app.Activity;
-
 import android.os.Bundle;
-
 import android.view.MotionEvent;
-import android.view.View;
-
 import android.view.ViewGroup;
 
-import android.widget.AutoCompleteTextView;
+public class LoginActivity extends Activity implements ZoomRootView.ZoomEventListener
+{
 
-import android.widget.EditText;
+	// UI references.
+	private ViewGroup mMainView;
 
+	private ZoomRootView zoomRootView;
 
-/**
- * A login screen that offers login via email/password.
- */
-public class LoginActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
 
+		mMainView = (ViewGroup) findViewById(R.id.main_view);
 
-    // UI references.
-     private ViewGroup mMainView;
+		zoomRootView = ZoomRootView.createZoomRootView(getApplicationContext(), mMainView, this);
 
+		//this doesn't work well with Viewpagers and some scrollable views
+		if (zoomRootView != null)
+		{
+			zoomRootView.triggerPreventDragClick(true);
+		}
 
-    private ZoomRootView zoomRootView;
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
-        mMainView = (ViewGroup)findViewById(R.id.main_view);
-
-        zoomRootView = ZoomRootView.createZoomRootView(getApplicationContext(),mMainView);
-
-        if(zoomRootView != null){
-            zoomRootView.triggerPreventDragClick(true);
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent motionEvent)
+	{
+        if (zoomRootView != null)
+        {
+            return zoomRootView.dispatchTouchWrapper(this, motionEvent);
         }
-
-    }
-
-
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-
-        if(!zoomRootView.dispatchTouchEvent(ev))
-            return super.dispatchTouchEvent(ev);
         else
-            return true;
-    }
+        {
+            return super.dispatchTouchEvent(motionEvent);
+        }
+	}
 
-
-
-
-
-
+	@Override
+	public boolean onDispatchTouchListener(MotionEvent motionEvent)
+	{
+		return super.dispatchTouchEvent(motionEvent);
+	}
 }
-
